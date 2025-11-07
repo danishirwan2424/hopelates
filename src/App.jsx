23d547d427d139b9b62116aa2ef386e409e24367
danishirwan2424 <danishirwaninfinite@gmail.com>
2025-11-07 18:58:22 +0800

    Update directory App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ function App() {
             }
           />
           <Route
-            path="/signup"
+            path="/signp"
             element={
               <PageWrapper>
                 <SignUp />

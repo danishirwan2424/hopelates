47df2aae1e5e00154746c66c70d29b91d394bf16
danishirwan2424 <danishirwaninfinite@gmail.com>
2025-11-18 18:18:20 +0800

    Update Donation Page

--- src/pages/staff/StaffDonation.jsx
+++ src/pages/staff/StaffDonation.jsx
@@ -132,7 +132,7 @@ function StaffDonation() {
                           <Search className="text-gray-500 w-5 h-5 mr-2" />
                           <input
                             type="text"
-                            placeholder="Search item name, donor..."
+                            placeholder="Search..."
                             className="bg-transparent outline-none w-full text-gray-700 placeholder-gray-500 text-sm"
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
